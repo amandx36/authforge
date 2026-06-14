c61d723da7eac0ab427fd76d5b891a16dae59a77
amandx36 <xamandeep360@gmail.com>
2026-06-14 22:11:32 +0530

    added route /forgot-password

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function VerifyEmail() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleForgotPass = async (e :any) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      const response = await axios.post(
+        "/api/users/verify-email",
+        {
+          email,
+        }
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+      alert("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Welcome to Verify Email Page</h2>
+
+      <form onSubmit={handleForgotPass}>
+        <label>Enter Email</label>
+
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Loading..." : "Verify Email"}
+        </button>
+      </form>
+    </div>
+  );
+}
