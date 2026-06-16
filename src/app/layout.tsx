7c8f42b45954dff7782cf42e129b3b0579afb53b
amandx36 <xamandeep360@gmail.com>
2026-06-16 09:01:41 +0530

    added toaster alert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Toaster } from 'react-hot-toast';
 export default function RootLayout({
   children,
 }: {
@@ -8,6 +8,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-dark text-white min-vh-100">
+        
+         <Toaster
+          position="top-right"
+          reverseOrder={false}
+        />
         {children}
       </body>
     </html>
