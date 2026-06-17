b3db974001b60b92aa350da9c1a9739e64fc367e
amandx36 <xamandeep360@gmail.com>
2026-06-17 06:52:00 +0530

    Now left is uploading the buffer into the cloudinay

--- src/app/api/users/upload-profile/route.ts
+++ src/app/api/users/upload-profile/route.ts
@@ -1,19 +1,45 @@
-import {
-  NextRequest,
-  NextResponse,
-} from "next/server";
+import cloudinary from "@/helper/cloudinary";
+import { NextRequest,NextResponse,} from "next/server";
 
 export async function POST( request: NextRequest) {
     try{
 
   const formData = await request.formData();
 
-     const image = formData.get("image");
+     const image = formData.get("image") as File;
 
   console.log("IMAGE:", image);
   console.log("Image recived")
+        if(!image){
+            return NextResponse.json ({
+                    error : "Error in uploading file"
+                    
+            },{
+                status:404
+            })
+        }
+    const arrayBuffer : any  = await image.arrayBuffer();
+    console.log("Converted to array buffer")
+    // convert array buffer  to buffer for cloudaniry 
+    const buffer = Buffer.from(arrayBuffer);
 
-  const arrayBuffer = 
+    console.log( "Converted to Buffer");
+        console.log(buffer.length);
+    
+    console.log("cloudinary name "+process.env.CLOUDINARY_CLOUD_NAME)
+    
+
+    return NextResponse.json(
+  {
+    success: true,
+    message: "Buffer created successfully",
+    size: buffer.length,
+  },
+  {
+    status: 200,
+  }
+);
+        
     }
     catch(error){
 
